2d35d62009ccc8b5afe0c773611b90d95c811a7a
Shoheb Pathan <shoheb.pathan@atyeti.com>
2025-10-08 12:19:39 +0530

    Mail sending with JMS(PUB-SUB)

---
+++ SpringBootMessaging/springbootJMSProject-2PublisherApplication/src/main/java/com/SpringbootJmsProject2PublisherApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJmsProject2PublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJmsProject2PublisherApplication.class, args);
+	}
+
+}
---
+++ SpringBootMessaging/springbootJMSProject-2PublisherApplication/src/test/java/com/example/springbootJMSProject_2PublisherApplication/SpringbootJmsProject2PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootJMSProject_2PublisherApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJmsProject2PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
